9711c4f641b74bfa9660f76e26a19ed9a06a3f36
Hugo Priori <priori.hugo@estudante.ufjf.br>
2022-01-05 20:48:36 -0300

    importando classe Cena

--- js/Cena.js
+++ js/Cena.js
@@ -1,9 +1,11 @@
-class Cena {
+export default class Cena {
 
+    ///responsavel por desenhar elementos em tela
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
     }
+    
     desenhar(){
         this.fillStyle = "black";
         this.ctx.fillRect(0 , 0 , this.canvas.width , this.canvas.heigh);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+import Cena from "./Cena.js" ;
+
+
 ///canvas config
 const canvas = document.body.querySelector("canvas");
 const ctx = canvas.getContext("2d")
+
+const c1 = new Cena(canvas);
+
+c1.desenhar();

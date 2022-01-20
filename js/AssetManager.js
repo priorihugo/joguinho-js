990e6f3b97bab8ed10188664664b87163616f8da
Hugo Priori <priori.hugo@estudante.ufjf.br>
2022-01-20 17:21:53 -0300

    Ajusta AssetManager para aceitar audio

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -4,28 +4,44 @@ export default class AssetManager{
         this.carregadas = 0;
         this.aCarregar = 0;
         this.imagens = new Map();
+        this.audios = new Map();
     }
     progresso(){
         if(this.aCarregar>0){
             return `${((this.carregadas/this.aCarregar)*100).toFixed(2)}`;
         }else{
-            return "nada a carregar";
+            return "[Nada a carregar]";
         }
     }
+
     carregaImagem(chave , src){
         const img = new Image();
         img.src = src;
-        img.addEventListener("laod" , ()=>{
-            console.log(`${this.carregadas}/${this.aCarregar} imagens carregadas`);
+        img.addEventListener("load" , ()=>{
+            console.log(`${this.carregadas}/${this.aCarregar} Imagens carregadas`);
             this.carregadas++;
         })
         this.imagens.set(chave, img);
         this.aCarregar++;
-
     }
     getImg(chave){
         return this.imagens.get(chave);
     }
+
+    carregaAudio(chave , src){
+        const audio = new Audio();
+        audio.src = src;
+        audio.addEventListener("load" , ()=>{
+            console.log(`${this.carregadas}/${this.aCarregar} Audios carregadas`);
+            this.carregadas++;
+        })
+        this.audios.set(chave, audio);
+        this.aCarregar++;
+    }
+    getAudio(chave){
+        return this.audios.get(chave);
+    }
+    
     acabou(){
         return this.carregadas === this.aCarregar;
     }
--- js/Cena.js
+++ js/Cena.js
@@ -1,72 +1,69 @@
 export default class Cena {
-
-    ///responsavel por desenhar elementos em tela
-    constructor(canvas, assets = null){
-        console.log("Criando cena...")
-        console.log(canvas)
-        this.canvas = canvas;
-        this.cw = canvas.width;
-        this.ch = canvas.heigh;
-        this.ctx = canvas.getContext("2d");
-        this.sprites = [];
-        this.t0 = null;
-        this.dt = null;
-        this.idAnim = null;
-        this.assets = assets;
-
+  ///responsavel por desenhar elementos em tela
+  constructor(canvas, assets = null) {
+    console.log("Criando cena...");
+    console.log(canvas);
+    this.canvas = canvas;
+    this.cw = canvas.width;
+    this.ch = canvas.heigh;
+    this.ctx = canvas.getContext("2d");
+    this.sprites = [];
+    this.t0 = null;
+    this.dt = null;
+    this.idAnim = null;
+    this.assets = assets;
+  }
+  desenhar() {
+    //console.log("Desenhando cena...")
+    //console.log(this.canvas)
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, this.cw, this.cw);
+    for (let s = 0; s < this.sprites.length; s++) {
+      let sprt = this.sprites[s];
+      sprt.desenhar(this.ctx);
     }
-    desenhar(){
-        //console.log("Desenhando cena...")
-        //console.log(this.canvas)
-        this.ctx.fillStyle = "black";
-        this.ctx.fillRect(0 , 0 , this.cw , this.cw);
-        for(let s = 0 ; s < this.sprites.length ; s++){
-            let sprt = this.sprites[s];
-            sprt.desenhar(this.ctx);
+    this.ctx.fillStyle = "yellow";
+    this.ctx.fillText(this.assets.progresso(), 40, 40);
+  }
+  adicionar(sprite) {
+    this.sprites.push(sprite);
+  }
+  passo(dt) {
+    for (const sprite of this.sprites) {
+      sprite.passo(dt);
+    }
+  }
+  quadro(t) {
+    if (this.assets.acabou()) {
+      this.t0 = this.t0 ?? t;
+      this.dt = (t - this.t0) / 1000;
+
+      this.passo(this.dt);
+      this.desenhar();
+      this.iniciar();
+
+      this.t0 = t;
+    }
+  }
+  iniciar() {
+    this.idAnim = requestAnimationFrame((t) => {
+      this.quadro(t);
+    });
+  }
+  parar() {
+    cancelAnimationFrame(this.idAnim);
+    this.t0 = null;
+    this.dt = null;
+  }
+
+  colisao() {
+    for (let a = 0; a < this.sprites.length - 1; a++) {
+      const sA = this.sprites[a];
+      for (let b = a; b < this.sprites.length; b++) {
+        const sB = this.sprites[b];
+        if (sA.colisaoCom(sB)) {
         }
-        this.ctx.fillStyle = "yellow";
-        this.ctx.fillText(this.assets.progresso(), 40 , 40)
+      }
     }
-    adicionar(sprite){
-        this.sprites.push(sprite);
-    }
-    passo(dt){
-        for (const sprite of this.sprites) {
-            sprite.passo(dt);        
-        }
-    }
-    quadro(t){
-        this.t0 = this.t0 ?? t;
-        this.dt = ( t - this.t0) / 1000;
-
-        this.passo(this.dt);
-        this.desenhar();
-        this.iniciar();
-
-        this.t0 = t;
-    }
-    iniciar(){
-        this.idAnim = requestAnimationFrame(
-            (t)=>{this.quadro(t)
-        });
-    }
-    parar(){
-        cancelAnimationFrame(this.idAnim);
-        this.t0 = null;
-        this.dt = null;
-    }
-
-    colisao(){
-        for(let a = 0 ; a < this.sprites.length - 1 ; a++){
-            const sA = this.sprites[a];
-            for(let b = a ; b < this.sprites.length ; b++){
-                const sB = this.sprites[b];
-                if(sA.colisaoCom(sB)){
-
-                }
-
-            }
-        }
-    }
-
+  }
 }
